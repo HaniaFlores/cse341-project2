b528c972ff0f1d1e8e2f2b7a20dd811a612e669c
Hania Flores <hanniaflores03@gmail.com>
2025-04-07 08:53:06 -0600

    added dependencies and dev/public swagger

--- swagger.js
+++ swagger.js
@@ -1,16 +1,25 @@
 const swaggerAutogen = require('swagger-autogen')();
 
-const doc = {
+const dev = {
     info: {
         title: 'To-Do App',
-        description: 'To-Do App'
+        description: 'Individual Project 2'
     },
     host: 'localhost:3000',
-    schemes: ['http', 'https']
+    schemes: ['http']
+};
+
+const public = {
+    info: {
+        title: 'To-Do App',
+        description: 'Individual Project 2'
+    },
+    host: 'project2-i6kb.onrender.com',
+    schemes: ['https']
 };
 
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];
 
 //this will generate swagger.json
-swaggerAutogen(outputFile, endpointsFiles, doc);
+swaggerAutogen(outputFile, endpointsFiles, dev);
