17a1c3b0ffd2459711d73bbe5ae9b6d6b6e186f7
Hania Flores <hanniaflores03@gmail.com>
2025-04-07 18:48:16 -0600

    added swagger tags

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,20 @@ const {isAuthenticated} = require('../middlewares/isAuthenticated');
 router.use('/', require('./swagger'));
 
 router.get('/', (req, res) => {
+/**
+ * #swagger.tags = ['Authentication']
+ *  #swagger.description = 'This endpoint retrieves the GitHub username.'
+*/
    res.send(req.session.user !== undefined
       ? `Logged in as ${req.session.user.displayName}`
       : 'Logged out');
 });
 
 router.get('/profile', isAuthenticated, (req, res) => {
+/**
+ * #swagger.tags = ['Authentication']
+ *  #swagger.description = 'This endpoint retrieves all GitHub user information.'
+*/
    res.json({
    message: 'Welcome to your profile!',
    user: req.session.user
